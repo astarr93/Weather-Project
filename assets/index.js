f76c5ad3f1d138f2f13b9b6f31af18ceab0d19e9
Andrew Starr <astarr93@outlook.com>
2020-07-14 03:23:39 -0400

    Rearranged script execution order. Updated Search Stylings.

--- assets/index.js
+++ assets/index.js
@@ -9,6 +9,25 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiYXN0YXJyOTMiLCJhIjoiY2tjam9md3FuMW5rbzJycWt1cWJsbWw5bSJ9.uq8v6j0Bgiv_8mxBBvvpmA'
 }).addTo(mymap);
 
+
+// When DOM is Loaded, begin:
+$(document).ready(function () {
+    addClickListener();
+})
+
+// Add Click Event Handler for Geo Location Button:
+function addClickListener() {
+    $('#searchGeoButton').on("click", function () {
+        console.log("success");
+    })
+}
+
+
+
 // L.tilelayer('http://maps.openweathermap.org/maps/2.0/weather/PAR0/13/{x}/{y}')
 // // OpenWeather API Key: bf9f438089a6bdeedce9b06784b29a58
 // MapBox Access Token: pk.eyJ1IjoiYXN0YXJyOTMiLCJhIjoiY2tjam9md3FuMW5rbzJycWt1cWJsbWw5bSJ9.uq8v6j0Bgiv_8mxBBvvpmA
+
+
+// Using simplemaps.com database for location lookup and longlat conversaion for OpenWeather
+// Need to create a modal that attriution for https://simplemaps.com/data/us-cities
